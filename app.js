cb5d260ba2730f80bc3158a7803b3619f97620dd
Constance <constance@MacBook-Pro-de-Constance.local>
2021-11-15 16:57:13 +0100

    cloudinary setup

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 require("dotenv").config();
-
+require("./configs/cloudinary");
 require("./configs/mongo");
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pinsRouter = require("./routes/pins");
 
 var app = express();
 
@@ -16,6 +19,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true, // usefull when dealing with authentication
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 1000* 60*60 * 4
+  },
+  resave: true,
+  saveUninitialized: true
+}))
+
 
 // Initialisation d'un user permanent son id doit être modifié quand on seed à nouveau
 app.use((req, res, next) => {
@@ -32,5 +54,7 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/api/pins", pinsRouter);
 
 module.exports = app;
+
---
+++ routes/pins.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const PinModel = require("./../models/Pin");
+const uploader = require("./../configs/cloudinary");
+
+// https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP
+// https://docs.microsoft.com/fr-fr/azure/architecture/best-practices/api-design
+
+// CREATE
+router.post("/", uploader.single("media"), async (req, res, next) => {
+  console.log(req.file);
+  try {
+    const newPin = await PinModel.create({ ...req.body, media: req.file.path }); //  req.file.path  => provided by cloudinary's response
+    res.status(201).json(newPin);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// READ
+
+router.get("/", async (req, res, next) => {
+  try {
+    const pins = await PinModel.find();
+    res.status(200).json(pins);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const pin = await PinModel.findById(req.params.id);
+    res.status(200).json(pin);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// UPDATE
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const updatedPin = await PinModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedPin);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedPin = await PinModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedPin);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
