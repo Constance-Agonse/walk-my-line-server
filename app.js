dad9adb9a81dadd0b1bfb670409cba5d8eaca448
Hugo Lansade <lhugo@lansade.com>
2021-11-15 12:41:20 +0100

    add a user that will always be connected

--- app.js
+++ app.js
@@ -15,7 +15,25 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.use((req, res, next) => {
+//   req.session.currentUser = {
+//     name : "Joe",
+//     nickname: "SuperJoe",
+//     email : "joe@joe.com",
+//     phone : "12",
+//     password :"multipass",
+//     rates : 2,
+//     _id: '6179280d72c4354791750fbf',
+//     skills : ["617929c9aa824a93bf7072ed",
+//   "61792a6f47ab974272eaebe1",
+// "61792c79ce26ab179839a215",
+// "61792e0887bf08eaf2fbe38c"]
+//   }
+//   next()
+// })
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
