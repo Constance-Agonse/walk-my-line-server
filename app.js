550854bd6dda9ba00cd762bc9d7d4b509b9d848c
Hugo Lansade <lhugo@lansade.com>
2021-11-16 16:47:16 +0100

    share to constance

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(session({
 // Initialisation d'un user permanent son id doit être modifié quand on seed à nouveau
 app.use((req, res, next) => {
   req.session.currentUser = {
-    _id : "61939315c511cf34f4062a40",
+    _id : "6193d1346c8c2d031c55cc9e",
     profilePic: "http://images6.fanpop.com/image/photos/39000000/Cool-Dog-animals-39056074-1600-900.jpg",
     isFollowing : [],
     username: "Croustie",
