ea27692f2e6cb28713987c505960fdba1762ff7d
Constance <constance@MacBook-Pro-de-Constance.local>
2021-11-20 15:29:07 +0100

    Revert "moved client application to server to be able to build"
    This reverts commit 397b02534fe1c3d11bc9616d42c05f7c7878c89e.

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "public/build")));
 
 
 
