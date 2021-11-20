50912a700a3023a3d55fa0ec93e66104a05e6a04
Hugo Lansade <lhugo@lansade.com>
2021-11-20 18:10:36 +0100

    follow btn is done for back

--- routes/createJourney.js
+++ routes/createJourney.js
@@ -34,28 +34,4 @@ router.get("/", async (req, res, next) => {
   });
 
 
-  // router.post("/",  async (req, res, next) => { //fileUploader.single("picture"),
-  //   try {
-  //     // get const variables from the req.body
-  //     const { name, description, isBand, styleName, color, wikiURL } = req.body;
-  //     // get the variable style from the select input (req.body)
-  //     let {style}  = req.body;
-  //     // Check if there is values to create a new style or use existing values from the select input
-  //     if (styleName !== "" && wikiURL !== "") {
-  //       const newStyle = await styleModel.create({
-  //         styleName,
-  //         wikiURL,
-  //         color,
-  //       });
-  //       //turn the new ID created in the DB into string
-  //       style = newStyle._id.toString();
-  //     }
-  //     //create the new artist with values from the inputs
-  //     const newArtist = await artistModel.create({name, description, isBand, style});
-  //     res.status(200).json(newArtist);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // });
-
 module.exports = router;
